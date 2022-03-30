02b4e4d0702069e0b32c2dab5d39fac4aca30fa5
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-03-30 23:12:19 +0900

    :lipstick: Feat: add styles for App.js
    add styles.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
 import memories from "./images/memories.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 
 export default function App() {
   return (
@@ -12,6 +14,23 @@ export default function App() {
         </Typography>
         <img src={memories} alt="memories" height="60" />
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 }
